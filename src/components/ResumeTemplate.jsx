d0f7345d07c49aca4be0b409bc2c5647f1edb91f
Tushar <singhtushar348@gmail.com>
2023-01-02 00:10:17 +0530

    fix social links and fonts for resume

--- src/components/ResumeTemplate.jsx
+++ src/components/ResumeTemplate.jsx
@@ -20,87 +20,71 @@ const ResumeTemplate = (props) => {
 
   const ref = React.useRef(null);
 
-  // const generatePdf = () => {
-  //   const doc = new jsPDF({
-  //     orientation: 'p',
-  //     format: "a4",
-  //     floatPrecision: 16,
-  //   });
-
-  //   doc.setFont("Times-Roman", "normal");
-
-  //   doc.html(ref.current, {
-  //     async callback(doc) {
-  //       await doc.save("resume");
-  //     },
-  //     autoPaging: "text",
-  //     margin: [20, 20, 20, 20],
-  //     x: 0,
-  //     y: 0,
-  //     width: 200,
-  //     windowWidth: 675
-  //   });
-  // };
-
   return (
     <>
-      <Stack spacing={4} ref={ref} m={6}>
-        <Center>
-          <Heading as="h1">
-            {resumeInfo.profile.firstname} {resumeInfo.profile.lastname}
-          </Heading>
-        </Center>
-        <Center>
-          <HStack
-            justify="center"
-            wrap="wrap"
-            maxW="60%"
-            divider={<StackDivider borderColor="gray.500" />}
-          >
-            {resumeInfo.profile.email.length ? (
-              <Link href={resumeInfo.profile.email} isExternal color="blue.500">
-                {" "}
-                {resumeInfo.profile.email}{" "}
-              </Link>
-            ) : null}
-            {resumeInfo.profile.linkedin.length ? (
-              <Link
-                href={resumeInfo.profile.linkedin}
-                isExternal
-                color="blue.500"
-              >
-                {" "}
-                {resumeInfo.profile.linkedin}{" "}
-              </Link>
-            ) : null}
-            {resumeInfo.profile.github.length ? (
-              <Link
-                href={resumeInfo.profile.github}
-                isExternal
-                color="blue.500"
-              >
-                {" "}
-                {resumeInfo.profile.github}{" "}
-              </Link>
-            ) : null}
-            {resumeInfo.profile.website.length ? (
-              <Link
-                href={resumeInfo.profile.website}
-                isExternal
-                color="blue.500"
-              >
-                {" "}
-                {resumeInfo.profile.website}{" "}
-              </Link>
-            ) : null}
+      <Stack spacing={4} ref={ref} m={6} fontFamily="sans-serif">
+        <Stack spacing={1}>
+          <Center>
+            <Heading as="h1">
+              {resumeInfo.profile.firstname} {resumeInfo.profile.lastname}
+            </Heading>
+          </Center>
+          <Center>
+            <HStack
+              justify="center"
+              wrap="wrap"
+              maxW="50%"
+              divider={<StackDivider borderColor="gray.500" />}
+            >
+              {resumeInfo.profile.email.length ? (
+                <Link
+                  href={resumeInfo.profile.email}
+                  isExternal
+                  color="blue.500"
+                >
+                  {" "}
+                  Email{" "}
+                </Link>
+              ) : null}
+              {resumeInfo.profile.linkedin.length ? (
+                <Link
+                  href={resumeInfo.profile.linkedin}
+                  isExternal
+                  color="blue.500"
+                >
+                  {" "}
+                  Linkedin{" "}
+                </Link>
+              ) : null}
+              {resumeInfo.profile.github.length ? (
+                <Link
+                  href={resumeInfo.profile.github}
+                  isExternal
+                  color="blue.500"
+                >
+                  {" "}
+                  Github{" "}
+                </Link>
+              ) : null}
+              {resumeInfo.profile.website.length ? (
+                <Link
+                  href={resumeInfo.profile.website}
+                  isExternal
+                  color="blue.500"
+                >
+                  {" "}
+                  Portfolio{" "}
+                </Link>
+              ) : null}
+            </HStack>
+          </Center>
+          <HStack justify="center">
+            <address>
+              <PhoneIcon /> {resumeInfo.profile.phone} &nbsp;
+              {resumeInfo.profile.address}
+            </address>
           </HStack>
-        </Center>
-        <HStack justify="center">
-          <address>
-            <PhoneIcon /> {resumeInfo.profile.phone} &nbsp;
-            {resumeInfo.profile.address}
-          </address>
-        </HStack>
+        </Stack>
         <VStack spacing={2} align="stretch">
           <Heading as="h3" fontSize="xl" borderBottomWidth="1px">
             SUMMARY
@@ -194,7 +178,15 @@ const ResumeTemplate = (props) => {
         </Button>
         <div>
           <ReactToPrint
-            trigger={() => <Button colorScheme="whatsapp" w="max-content" isDisabled={page !== 3}>Print this out!</Button>}
+            trigger={() => (
+              <Button
+                colorScheme="whatsapp"
+                w="max-content"
+                isDisabled={page !== 3}
+              >
+                Print this out!
+              </Button>
+            )}
             content={() => ref.current}
           />
         </div>
